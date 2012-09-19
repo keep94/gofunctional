f32241557afb32d474ef096df9bfa66fe4ed7c8a
Travis Keep <keep94@gmail.com>
2012-09-19 05:39:44 -0700

    Add Consumer interface and associated functions.

---
+++ functional/consumers.go
@@ -0,0 +1,102 @@
+package functional
+
+// A Consumer of T consumes the T values from a Stream of T.
+type Consumer interface {
+
+  // Consume consumes values from Stream s
+  Consume(s Stream)
+}
+
+// ModifyConsumerStream returns a new Consumer that applies f to its Stream
+// and then gives the result to c. If c is a Consumer of T and f takes a
+// Stream of U and returns a Stream of T, then ModifyConsumerStream returns a
+// Consumer of U.
+func ModifyConsumerStream(c Consumer, f func(s Stream) Stream) Consumer {
+  return &modifiedConsumerStream{c, f}
+}
+
+// MultiConsume consumes the values of s, a Stream of T, sending those T
+// values to each Consumer in consumers. MultiConsume consumes values from s
+// until either s is exhausted or until no Consumer in consumers is accepting
+// values. ptr is a *T that receives the values from s. copier is a Copier
+// of T used to copy T values to the Streams sent to each Consumer in
+// consumers. Passing null for copier means use simple assignment.
+func MultiConsume(s Stream, ptr interface{}, copier Copier, consumers ...Consumer) {
+  if copier == nil {
+    copier = assignCopier
+  }
+  streams := make([]*splitStream, len(consumers))
+  stillConsuming := false
+  for i := range streams {
+    streams[i] = &splitStream{ptrCh: make(chan interface{}), nextReturnCh: make(chan bool)}
+    go consumerWrapper(streams[i], consumers[i])
+    if streams[i].cleanupIfDone() {
+      stillConsuming = true
+    }
+  }
+  for stillConsuming && s.Next(ptr) {
+    stillConsuming = false
+    for i := range streams {
+      p := streams[i].currentPtr()
+      if p != nil {
+        copier(ptr, p)
+      }
+      if streams[i].nextReturn(true) {
+        stillConsuming = true
+      }
+    }
+  }
+  for i := range streams {
+    streams[i].nextReturn(false)
+  }
+}
+
+type modifiedConsumerStream struct {
+  c Consumer
+  f func(s Stream) Stream
+}
+
+func (mc *modifiedConsumerStream) Consume(s Stream) {
+  mc.c.Consume(mc.f(s))
+}
+
+func consumerWrapper(s *splitStream, c Consumer) {
+  c.Consume(s)
+  s.ptrCh <- nil
+}
+
+type splitStream struct {
+  ptrCh chan interface{}
+  nextReturnCh chan bool
+  ptr interface{}
+}
+
+func (s *splitStream) Next(ptr interface{}) bool {
+  s.ptrCh <- ptr
+  return <-s.nextReturnCh
+}
+
+func (s *splitStream) currentPtr() interface{} {
+  return s.ptr
+}
+
+func (s *splitStream) nextReturn(returnValue bool) bool {
+  if s.nextReturnCh == nil {
+    return false
+  }
+  s.nextReturnCh <- returnValue
+  return s.cleanupIfDone()
+}
+
+func (s *splitStream) cleanupIfDone() bool {
+  s.ptr = <-s.ptrCh
+  if s.ptr == nil {
+    close(s.ptrCh)
+    close(s.nextReturnCh)
+    s.ptrCh = nil
+    s.nextReturnCh = nil
+    return false
+  }
+  return true
+}
+
---
+++ functional/consumers_test.go
@@ -0,0 +1,81 @@
+package functional
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestNormal(t *testing.T) {
+  s := Slice(Count(), 0, 5)
+  ec := newEvenNumberConsumer()
+  oc := newOddNumberConsumer()
+  MultiConsume(s, new(int), nil, ec, oc)
+  if output := fmt.Sprintf("%v", ec.results); output != "[0 2 4]" {
+    t.Errorf("Expected [0 2 4] got %v", output)
+  }
+  if output := fmt.Sprintf("%v", oc.results); output != "[1 3]" {
+    t.Errorf("Expected [1 3] got %v", output)
+  }
+}
+
+func TestConsumersEndEarly(t *testing.T) {
+  s := Count()
+  nc := ModifyConsumerStream(newEvenNumberConsumer(), func(s Stream) Stream {
+    return NilStream()
+  })
+  first5 := func(s Stream) Stream {
+    return Slice(s, 0, 5)
+  }
+  ec := newEvenNumberConsumer()
+  oc := newOddNumberConsumer()
+  MultiConsume(
+      s,
+      new(int),
+      nil,
+      nc,
+      ModifyConsumerStream(ec, first5),
+      ModifyConsumerStream(oc, first5))
+  if output := fmt.Sprintf("%v", ec.results); output != "[0 2 4]" {
+    t.Errorf("Expected [0 2 4] got %v", output)
+  }
+  if output := fmt.Sprintf("%v", oc.results); output != "[1 3]" {
+    t.Errorf("Expected [1 3] got %v", output)
+  }
+  var result int
+  s.Next(&result)
+  if result != 5 {
+    t.Errorf("Expected 5 got %v", result)
+  }
+}
+
+func TestNoConsumers(t *testing.T) {
+  s := CountFrom(7, 1)
+  MultiConsume(s, new(int), nil)
+  var result int
+  if !s.Next(&result) || result != 7 {
+    t.Errorf("Expected 7 got %v", result)
+  }
+}
+
+type filterConsumer struct {
+  f Filterer
+  results []int
+}
+
+func (fc *filterConsumer) Consume(s Stream) {
+  AppendValues(Filter(fc.f, s), &fc.results)
+}
+
+func newEvenNumberConsumer() *filterConsumer {
+  return &filterConsumer{f: NewFilterer(func(ptr interface{}) bool {
+    p := ptr.(*int)
+    return *p % 2 == 0
+  })}
+}
+
+func newOddNumberConsumer() *filterConsumer {
+  return &filterConsumer{f: NewFilterer(func(ptr interface{}) bool {
+    p := ptr.(*int)
+    return *p % 2 == 1
+  })}
+}
